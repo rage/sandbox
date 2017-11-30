5289d887197732bf0ad9fa11148e5fa26355788d
Henrik Nygren <nygrenh@gmail.com>
2017-11-30 19:14:23 +0200

    Command for running the tests

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -eu
+
+ID=$(uuidgen)
+OUTPUT_PATH="$ID"_test_ouput.txt
+
+# docker build . -t nygrenh/sandbox-next
+
+docker create --name "$ID" -i nygrenh/sandbox-next
+docker cp test-exercise/. "$ID":/app
+docker start -i "$ID"
+docker cp "$ID":/app/test_output.txt "$OUTPUT_PATH"
+python3 -m json.tool < $OUTPUT_PATH
+rm "$OUTPUT_PATH"
+docker rm "$ID"
